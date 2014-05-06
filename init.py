de79ad174d93d479cea2c7e9813115ddd5287875
YarushinNA <nikita_yarushin@mail.ru>
2014-05-06 17:49:02 +0600

    Фухх, версия 0.0.0.0.2a

--- init.py
+++ init.py
@@ -1,27 +1,31 @@
 # -*- coding: utf-8 -*-
 import csv
 import os
-import datetime
 import time
+import sys
+
+
 def timer(f):
     """Таймер функции"""
+    coding = sys.stdin.encoding
+
     def tmp(*args, **kwargs):
         t = time.time()
         res = f(*args, **kwargs)
-        print "Время выполнения функции: %f" % (time.time()-t)
+        print "Время выполнения функции: %f".decode('UTF-8').encode(coding) % (time.time()-t)
         return res
-
     return tmp
-#writer = csv.writer(open("some.csv", "wb"), delimiter=',', quoting=csv.QUOTE_MINIMAL,
-#                    quotechar='#', lineterminator='\n')
+
+
 def getfilelist(mesyaz, den, god):
+    coding = sys.stdin.encoding
     if mesyaz == '':
         mesyaz = '05'
     if den == '':
         den = '05'
     if god == '':
         god = '2014'
-    usluga = raw_input('Введите услугу(А, М, Д):')
+    usluga = raw_input('Введите услугу(А, М, Д):'.decode('UTF-8').encode(coding))
     dpap = god + '_' + mesyaz + '_' + den
     if usluga == 'А':
         papka = 'D:\work\_Chelinvestbank\_Из банка\Архив' '\\' + dpap + '\\' 'альт'
@@ -36,24 +40,28 @@ def getfilelist(mesyaz, den, god):
     files = [papka.decode('utf-8') + '\\' + fil for fil in files]
     return files
 
+
 @timer
-def search_csv(files, searchstr):
+def search_csv(files, search_str):
     for fil in files:
 #        print fil
         reader = csv.reader(open(fil, "rb"))
         for row in reader:
             row = "".join(row).decode('cp866')
-            if searchstr in row:
+            if search_str in row:
+                print fil
                 print row
 
 
-
 def main():
-    mesyaz = raw_input('Введите нужный месяц:')
-    den = raw_input('Введите нужный день:')
-    god = raw_input('Введите нужный год:')
-    searchstr = raw_input('Введите искомую строку:')
-    search_csv(getfilelist(mesyaz, den, god), searchstr)
+    coding = sys.stdin.encoding
+    den = raw_input('Введите нужный день:'.decode('UTF-8').encode(coding))
+    mesyaz = raw_input('Введите нужный месяц:'.decode('UTF-8').encode(coding))
+    god = raw_input('Введите нужный год:'.decode('UTF-8').encode(coding))
+    search_str = raw_input('Введите искомую строку:'.decode('UTF-8').encode(coding))
+    search_csv(getfilelist(mesyaz, den, god), search_str)
+    a = raw_input('')
+    print a
 
 if __name__ == "__main__":
     main()
