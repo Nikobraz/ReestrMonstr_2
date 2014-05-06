fe19aa40cca8863a322ed35e74d5a370164568f2
YarushinNA <nikita_yarushin@mail.ru>
2014-05-06 14:43:31 +0600

    Фухх, версия 0.0.0.0.1a

--- init.py
+++ init.py
@@ -2,34 +2,58 @@
 import csv
 import os
 import datetime
+import time
+def timer(f):
+    """Таймер функции"""
+    def tmp(*args, **kwargs):
+        t = time.time()
+        res = f(*args, **kwargs)
+        print "Время выполнения функции: %f" % (time.time()-t)
+        return res
+
+    return tmp
 #writer = csv.writer(open("some.csv", "wb"), delimiter=',', quoting=csv.QUOTE_MINIMAL,
 #                    quotechar='#', lineterminator='\n')
-def getfilelist():
-    mesyaz = raw_input('Введите нужный месяц:')
-    den = raw_input('Введите нужный день:')
-    god = raw_input('Введите нужный год:')
+def getfilelist(mesyaz, den, god):
+    if mesyaz == '':
+        mesyaz = '05'
+    if den == '':
+        den = '05'
+    if god == '':
+        god = '2014'
     usluga = raw_input('Введите услугу(А, М, Д):')
     dpap = god + '_' + mesyaz + '_' + den
     if usluga == 'А':
-        files = 'D:\work\_Chelinvestbank\_Из банка\Архив' + '\\' + dpap + '\\' + 'альт'
+        papka = 'D:\work\_Chelinvestbank\_Из банка\Архив' '\\' + dpap + '\\' 'альт'
     elif usluga == 'M':
-        files = 'D:\work\_Chelinvestbank\_Из банка\Архив' + '\\' + dpap + '\\' + 'мастер'
+        papka = 'D:\work\_Chelinvestbank\_Из банка\Архив' '\\' + dpap + '\\' 'мастер'
     elif usluga == 'Д':
-        files = 'D:\work\_Chelinvestbank\_Из банка\Архив' + '\\' + dpap + '\\' + 'дез'
+        papka = 'D:\work\_Chelinvestbank\_Из банка\Архив' '\\' + dpap + '\\' 'дез'
     else:
-        files = 'D:\work\_Chelinvestbank\_Из банка\Архив' + '\\' + dpap
-    files = os.listdir(files.decode('utf-8'))
+        papka = 'D:\work\_Chelinvestbank\_Из банка\Архив' '\\' + dpap
+    files = os.listdir(papka.decode('utf-8'))
     files = filter(lambda x: x.endswith('.txt'), files)
+    files = [papka.decode('utf-8') + '\\' + fil for fil in files]
     return files
 
-
-def read_csv(files):
+@timer
+def search_csv(files, searchstr):
     for fil in files:
+#        print fil
         reader = csv.reader(open(fil, "rb"))
-
         for row in reader:
             row = "".join(row).decode('cp866')
-            row = row.decode('cp866')
-            print row,
+            if searchstr in row:
+                print row
 
-read_csv(getfilelist())
+
+
+def main():
+    mesyaz = raw_input('Введите нужный месяц:')
+    den = raw_input('Введите нужный день:')
+    god = raw_input('Введите нужный год:')
+    searchstr = raw_input('Введите искомую строку:')
+    search_csv(getfilelist(mesyaz, den, god), searchstr)
+
+if __name__ == "__main__":
+    main()
